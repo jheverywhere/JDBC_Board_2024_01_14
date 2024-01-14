04b1968ce6809bf5ee0cb6886f1540157cb9e2c9
jheverywhere <nighteolt@naver.com>
2024-01-14 14:05:26 +0900

    게시물 리스팅,추가 기능 구현

---
+++ src/main/java/com/jh/jdbc/board/App.java
@@ -0,0 +1,62 @@
+package com.jh.jdbc.board;
+
+import java.util.*;
+
+public class App {
+  int articleLastId;
+  List<Article> articles;
+
+  public App() {
+
+    articleLastId = 0;
+    articles = new ArrayList<>();
+  }
+
+  public void run(){
+
+    System.out.println("=== JDBC게시판 시작 ===");
+
+    Scanner in = new Scanner(System.in);
+
+    while(true){
+      System.out.printf("명령) ");
+      String cmd = in.nextLine();
+      if(cmd.equals("/usr/article/write")){
+        System.out.printf("제목 : ");
+        String title = in.nextLine();
+        System.out.printf("내용 : ");
+        String body = in.nextLine();
+
+        int id = ++articleLastId;
+        articles.add(new Article(id,title,body));
+
+        System.out.println("입력된 게시물 : " + articles.get(id-1));
+        System.out.printf("%d번 게시물이 등록되었습니다.\n",id);
+      }else if(cmd.equals("exit")){
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }else if(cmd.equals("/usr/article/list")){
+        System.out.println("===게시물 리스트===");
+        System.out.println("번호 // 제목 // 내용");
+
+        /* 오름차순 정렬
+        for(Article article : articles){
+          System.out.printf("%d // %s // %s \n",article.id,article.title,article.body);
+        }
+        */
+
+        //내림차순 정렬
+        for (int i = articles.size()-1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d // %s // %s \n",article.id , article.title,article.body);
+        }
+
+
+      }
+      else{
+        System.out.println("명령어를 잘못 입력하셨습니다. 다시 입력해주세요.");
+      }
+
+    }
+  }
+}
--- src/main/java/com/jh/jdbc/board/Main.java
+++ src/main/java/com/jh/jdbc/board/Main.java
@@ -1,35 +1,13 @@
 package com.jh.jdbc.board;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int id = 0;
-
-
-    System.out.println("=== JDBC게시판 시작 ===");
-    while(true){
-      System.out.printf("명령) ");
-      String cmd = in.nextLine();
-      if(cmd.equals("/usr/article/write")){
-        System.out.printf("제목 : ");
-        String title = in.nextLine();
-        System.out.printf("내용 : ");
-        String body = in.nextLine();
-       id++;
-        System.out.printf("%d번 게시물이 등록되었습니다.\n",id);
-      }else if(cmd.equals("exit")){
-        System.out.println("프로그램을 종료합니다.");
-        break;
-      }
-
-    }
-
-
-
-
+    new App().run();
 
   }
 }
