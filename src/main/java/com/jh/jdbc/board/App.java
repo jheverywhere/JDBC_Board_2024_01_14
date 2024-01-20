5d0f3dcf36b6206eb5fae26cb1cca87912393958
jheverywhere <nighteolt@naver.com>
2024-01-21 00:05:08 +0900

    게시물 리스트 데이터를 DB에서 가져오기

--- src/main/java/com/jh/jdbc/board/App.java
+++ src/main/java/com/jh/jdbc/board/App.java
@@ -1,9 +1,6 @@
 package com.jh.jdbc.board;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
 
 public class App {
@@ -92,24 +89,96 @@ public class App {
         break;
       }else if(cmd.equals("/usr/article/list")){
 
+
+// JDBC 드라이버 클래스 이름
+        String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+
+        // 데이터베이스 연결 정보
+        String url = "jdbc:mysql://localhost:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+        String username = "jheverywhere";
+        String password = "jh960525";
+
+        Connection conn = null;
+        PreparedStatement pstat = null;
+        ResultSet rs = null;
+
+
+        try {
+          // JDBC 드라이버 로드
+          Class.forName(jdbcDriver);
+
+          // 데이터베이스에 연결
+          conn = DriverManager.getConnection(url, username, password);
+
+          String sql = "SELECT *";
+          sql += " FROM article";
+          sql += " ORDER BY id DESC;";
+
+          pstat = conn.prepareStatement(sql);
+          rs = pstat.executeQuery(sql);
+
+          while(rs.next()){
+            int id = rs.getInt("id");
+            String regDate = rs.getString("regDate");
+            String updateDate = rs.getString("updateDate");
+            String title = rs.getString("title");
+            String body = rs.getString("body");
+
+            Article article = new Article(id,regDate,updateDate,title,body);
+            articles.add(article);
+          }
+
+          System.out.println("결과 : "+ articles);
+
+        } catch (ClassNotFoundException e) {
+          System.out.println("드라이버 로딩 실패");
+        } catch (SQLException e) {
+          System.out.println("에러 : " + e);
+        } finally {
+          try {
+            if (rs != null && !rs.isClosed()) {
+              rs.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (pstat != null && !pstat.isClosed()) {
+              pstat.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+          try {
+            if (conn != null && !conn.isClosed()) {
+              // 연결 닫기
+              conn.close();
+            }
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+
+        }
+
         if(articles.isEmpty()){
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
-        System.out.println("===게시물 리스트===");
-        System.out.println("번호 // 제목 // 내용");
 
-        /* 오름차순 정렬
+        System.out.println("===게시물 리스트===");
+        System.out.println("번호 / 제목");
+
+
         for(Article article : articles){
           System.out.printf("%d // %s // %s \n",article.id,article.title,article.body);
         }
-        */
+
 
         //내림차순 정렬
-        for (int i = articles.size()-1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf("%d // %s // %s \n",article.id , article.title,article.body);
-        }
+//        for (int i = articles.size()-1; i >= 0; i--) {
+//          Article article = articles.get(i);
+//          System.out.printf("%d / %s  \n",article.id , article.title);
+//        }
 
 
       }
---
+++ src/test/java/com/jh/jdbc/test/JDBCUpdateTest.java
@@ -0,0 +1,83 @@
+package com.jh.jdbc.test;
+
+
+import com.jh.jdbc.board.Article;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCUpdateTest {
+  public static void main(String[] args) {
+    // JDBC 드라이버 클래스 이름
+    String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+
+    // 데이터베이스 연결 정보
+    String url = "jdbc:mysql://localhost:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+    String username = "jheverywhere";
+    String password = "jh960525";
+
+    Connection conn = null;
+    PreparedStatement pstat = null;
+    ResultSet rs = null;
+
+    List<Article>articles = new ArrayList<>();
+
+    try {
+      // JDBC 드라이버 로드
+      Class.forName(jdbcDriver);
+
+      // 데이터베이스에 연결
+      conn = DriverManager.getConnection(url, username, password);
+
+     String sql = "SELECT *";
+     sql += " FROM article";
+     sql += " ORDER BY id DESC;";
+
+      pstat = conn.prepareStatement(sql);
+      rs = pstat.executeQuery(sql);
+
+      while(rs.next()){
+        int id = rs.getInt("id");
+        String regDate = rs.getString("regDate");
+        String updateDate = rs.getString("updateDate");
+        String title = rs.getString("title");
+        String body = rs.getString("body");
+
+        Article article = new Article(id,regDate,updateDate,title,body);
+        articles.add(article);
+      }
+
+      System.out.println("결과 : "+ articles);
+
+    } catch (ClassNotFoundException e) {
+      System.out.println("드라이버 로딩 실패");
+    } catch (SQLException e) {
+      System.out.println("에러 : " + e);
+    } finally {
+      try {
+        if (rs != null && !rs.isClosed()) {
+          rs.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      try {
+        if (pstat != null && !pstat.isClosed()) {
+          pstat.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      try {
+        if (conn != null && !conn.isClosed()) {
+          // 연결 닫기
+          conn.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+    }
+  }
+}
