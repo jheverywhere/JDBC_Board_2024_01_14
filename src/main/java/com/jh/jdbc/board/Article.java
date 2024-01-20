ba33637822d48dc651fcaab3b048b2ed70a3aeba
jheverywhere <nighteolt@naver.com>
2024-01-20 23:51:39 +0900

    SELECT 테스트

--- src/main/java/com/jh/jdbc/board/Article.java
+++ src/main/java/com/jh/jdbc/board/Article.java
@@ -2,8 +2,18 @@ package com.jh.jdbc.board;
 
 public class Article {
  public int id;
+ public String regDate;
+ public String updateDate;
  public String title;
  public String body;
+
+ public Article(int id, String regDate, String updateDate, String title, String body) {
+  this.id = id;
+  this.regDate = regDate;
+  this.updateDate = updateDate;
+  this.title = title;
+  this.body = body;
+ }
 
  public Article(int id, String title, String body) {
   this.id = id;
