e428a7bfe92b1112f9e851d3527cbfcfe930e938
jheverywhere <nighteolt@naver.com>
2024-01-20 22:00:14 +0900

    메이븐 리포지터리에 mysql connector 적용

--- src/main/java/com/jh/jdbc/board/App.java
+++ src/main/java/com/jh/jdbc/board/App.java
@@ -36,6 +36,11 @@ public class App {
         System.out.println("프로그램을 종료합니다.");
         break;
       }else if(cmd.equals("/usr/article/list")){
+
+        if(articles.isEmpty()){
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
         System.out.println("===게시물 리스트===");
         System.out.println("번호 // 제목 // 내용");
 
